ed6e0f60792a52f19593931d30ce204a6948d8fc
Sadir Menetal <sadirmenetal@gmail.com>
2017-05-03 01:08:32 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -8,21 +8,12 @@ exports.getImage = function(limit, tags, callback){
 
     var address = `http://gelbooru.com/index.php?page=dapi&s=post&q=index&limit=${limit}&tags=${tags}`;
     agent.post(address).end(function (err, res) {
-
-        let cleanedString = res.text.replace("\ufeff", "");
-        parseString(cleanedString, function (err, result) {
-            if (err)
-                console.log("Error", err);
-            else {
-                console.log(result.posts.post[0].$.file_url);
-                var output = result.posts.post[0].$.file_url;
-                callback("http:" + output);
-            }
-        });
+        callback(res);
 
         if (err) {
             console.log(err);
             callback(res);
         }
+
     });
 };
--- test.js
+++ test.js
@@ -2,15 +2,13 @@ const gelbooru = require('./index');
 const parseString = require('xml2js').parseString;
 
 gelbooru.getImage(1, "forced_orgasm", function (data) {
-    //let cleanedString = data.text.replace("\ufeff", "");
-    //parseString(cleanedString, function (err, result) {
-    //    if (err)
-    //        console.log("Error", err);
-    //    else {
-    //        console.log(result.posts);
-    //        var output = result.posts.post[0].$.file_url;//.replace("//","");
-    //        // callback("http://"+output);	
-    //        callback("http:" + output);
-    //    }
-    //});
+    let cleanedString = data.text.replace("\ufeff", "");
+    parseString(cleanedString, function (error, result) {
+        if (error)
+            console.log("Error", error);
+        else {
+            console.log(result.posts.post[0].$.file_url);
+            var imageURL = result.posts.post[0].$.file_url;
+        }
+    });
 });
